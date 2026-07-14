5a4cc76e5a24a9f7452e58b5bc60f839cdd08f4c
鸡好 <rutin_chicken@163.com>
2026-07-14 22:00:31 +0800

    feat: 添加默认颜色数组，包含多种颜色及其名称

---
+++ material小工具系列/dat.js
@@ -0,0 +1,15 @@
+const DefaultColors = [    
+    { hex: 'FF0000', name: '标准红色'},
+    { hex: '00FF00', name: '标准绿色'},
+    { hex: '0000FF', name: '标准蓝色'},
+    { hex: '86EAC8', name: '薄荷绿' },
+    { hex: 'A7F3D0', name: '淡翡翠' },
+    { hex: 'A6E0CF', name: '淡绿' },
+    { hex: 'F4C2C2', name: '淡玫瑰' },
+    { hex: '01847F', name: '马尔斯' },
+    { hex: 'FFD1DC', name: '嫩粉色' },
+    { hex: 'CFE2F3', name: '浅灰蓝' },
+    { hex: '87CEEB', name: '天蓝' },
+    { hex: 'FFB7C5', name: '樱花粉' },
+    { hex: 'EDF7FA', name: '月光蓝' }
+];
